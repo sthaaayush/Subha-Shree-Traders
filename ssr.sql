6892a9f2711335248117469ed83320032d89070a
Aayush Shresth <aayushstha20@gmail.com>
2024-05-28 15:03:44 +0545

    updated database

--- ssr.sql
+++ ssr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2024 at 06:53 PM
+-- Generation Time: May 28, 2024 at 11:17 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `shop_db`
+-- Database: `ssr`
 --
-CREATE DATABASE IF NOT EXISTS `shop_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `shop_db`;
+CREATE DATABASE IF NOT EXISTS `ssr` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `ssr`;
 
 -- --------------------------------------------------------
 
@@ -40,7 +40,8 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `password`) VALUES
-(1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2');
+(1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2'),
+(0, 'aayush', '9c3224585bd58adf5f25defd8eeef5de9128521d');
 
 -- --------------------------------------------------------
 
@@ -93,13 +94,6 @@ CREATE TABLE `messages` (
   `message` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`id`, `user_id`, `name`, `email`, `number`, `message`) VALUES
-(1, 0, 'Aayush', 'aayushstha20@gmail.com', '986565656', 'Hy, my product hasn&#39;t delivered till now');
-
 -- --------------------------------------------------------
 
 --
@@ -125,8 +119,11 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
-(3, 1, 'Aayush Shrestha', '9865656561', 'aayushstha20@gmail.com', 'cash on delivery', 'flat no. House 4, New road, Kathamandu, Bagmati, Nepal - 56577', 'Digital Camera (100 x 4) - 6 gang + socket switch (230 x 4) - ', 1320, '2024-05-17', 'pending'),
-(4, 1, 'Aayush Shrestha', '9865656561', 'kanti@gmail.com', 'cash on delivery', 'flat no. House 4, New road, Kathamandu, Bagmati, Nepal - 435345', 'Antique Wall Bulb (450 x 3)', 1350, '2024-05-17', 'pending');
+(4, 1, 'Aayush Shrestha', '9865656561', 'kanti@gmail.com', 'cash on delivery', 'flat no. House 4, New road, Kathamandu, Bagmati, Nepal - 435345', 'Antique Wall Bulb (450 x 3)', 1350, '2024-05-17', 'pending'),
+(5, 2, 'Aayush Shrestha', '9865656561', 'aayushstha20@gmail.com', 'cash on delivery', 'flat no. House 4, New road, Biratnagar, Lumbini, Nepal - 243242', 'Antique Wall Lamp (450 x 2)', 900, '2024-05-19', 'completed'),
+(6, 1, 'Aayush Shrestha', '9865656561', 'aayushstha20@gmail.com', 'cash on delivery', 'flat no. House 4, New road, Kathamandu, Bagmati, Nepal - 323423', 'Digital Camera (2000 x 20)', 40000, '2024-05-19', 'pending'),
+(7, 2, 'Aayush Shrestha', '9865656561', 'aayushstha20@gmail.com', 'cash on delivery', 'flat no. House 4, New road, Biratnagar, Bagmati, Nepal - 324234', '6 gang + socket switch (230 x 1), Antique Wall Lamp (450 x 5)', 2480, '2024-05-20', 'pending'),
+(8, 1, 'Aayush Shrestha', '9865656561', 'john.doe@example.com', 'cash on delivery', 'flat no. House 4, New road, Kathamandu, Bagmati, Nepal - 342342', 'Antique Wall Lamp (450 x 10)', 4500, '2024-05-28', 'completed');
 
 -- --------------------------------------------------------
 
@@ -151,9 +148,34 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `details`, `price`, `image_01`, `image_02`, `image_03`, `category_id`, `quantity`) VALUES
-(3, 'Digital Camera', 'Clicks good quality pics', 100, 'camera-1.webp', 'camera-2.webp', 'camera-3.webp', 1, 20),
-(4, 'Antique Wall Bulb', 'Antique Design Wall Light With Bulb with aesthetic lightining ', 450, 'Antique wall bulb1.png', 'Antique wall bulb2.png', 'Antique wall bulb3.png', 5, 7),
-(5, '6 gang + socket switch', 'Durable and white in color', 230, 'G gang socket2.png', 'G gang socket.png', '', 6, 20);
+(4, 'Antique Wall Lamp', 'Antique Design Wall Light With Bulb with aesthetic lightining ', 450, 'Antique wall bulb1.png', 'Antique wall bulb2.png', 'Antique wall bulb3.png', 5, 10),
+(5, '6 gang + socket switch', 'Durable and white in color', 150, 'G gang socket2.png', 'G gang socket.png', '', 6, 19),
+(6, 'Extension cord', 'Very good\r\n', 200, 'extension cord.png', '', '', 6, 10),
+(7, 'Universal Extension Board', 'Reliable nd durable', 260, 'universal extension cord2.png', 'universal extension cord.png', '', 6, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `product_id` int(100) NOT NULL,
+  `rating` int(1) NOT NULL CHECK (`rating` >= 1 and `rating` <= 5),
+  `comment` varchar(1000) NOT NULL,
+  `review_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `user_id`, `product_id`, `rating`, `comment`, `review_date`) VALUES
+(14, 2, 4, 4, 'klmlkjo', '2024-05-19 12:28:01'),
+(15, 1, 4, 4, 'dasdasdas', '2024-05-19 20:03:37'),
+(16, 2, 5, 4, 'f;fnkkaksda  aka\'sk', '2024-05-20 13:51:31');
 
 -- --------------------------------------------------------
 
@@ -173,7 +195,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Aayush', 'aayushstha20@gmail.com', '9c3224585bd58adf5f25defd8eeef5de9128521d');
+(1, 'Aayush', 'aayushstha20@gmail.com', '9c3224585bd58adf5f25defd8eeef5de9128521d'),
+(2, 'Ram', 'ram@gmail.com', 'e17e5425a021224b63e91499ff8ac491c87567db');
 
 -- --------------------------------------------------------
 
@@ -226,6 +249,14 @@ ALTER TABLE `products`
   ADD KEY `fk_category` (`category_id`);
 
 --
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -245,43 +276,49 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -292,6 +329,13 @@ ALTER TABLE `wishlist`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
